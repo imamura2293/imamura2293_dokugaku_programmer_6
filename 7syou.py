52fd97c7688fd181d605cc98c321e51156a44bc3
imamura2293 <uu77ee22dd55>
2020-09-07 00:57:51 +0900

    7章

--- 7syou.py
+++ 7syou.py
@@ -84,3 +84,31 @@ while True:
     if a == "q":
         break
     n = (n + 1) %　3
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+i = 1
+while i <= 5:
+    if i == 3:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+for i in range(1, 3):
+    print(i)
+    for letter in ["a", "b", "c"]:
+        print(letter)
+
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+added = []
+for i in list1:
+    for j in list2:
+
+while input('y or n') ! ='n':
+    for i in range(1, 6):
+        print(i)
